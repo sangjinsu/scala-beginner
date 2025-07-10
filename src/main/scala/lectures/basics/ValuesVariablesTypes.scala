98d5017fd5313297fa894b32cdbe7818337145dc
sangjinsu <climbplant39@kakao.com>
2025-07-10 23:34:41 +0900

    ValuesVariablesTypes

---
+++ src/main/scala/lectures/basics/ValuesVariablesTypes.scala
@@ -0,0 +1,21 @@
+package lectures.basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+
+  println(x)
+
+  val y: String = "Hello scala"
+  println(y)
+
+  val aBoolean : Boolean = false
+  val aChar : Char = 'a'
+  val anInt: Int = x 
+  val aShort : Short = 4613
+  val aLong : Long = 24123412341325L
+  val aFloat : Float = 2.0f
+  val aDouble : Double = 3.14
+  
+  var aVariable: Int = 4
+  aVariable = 5
+}
